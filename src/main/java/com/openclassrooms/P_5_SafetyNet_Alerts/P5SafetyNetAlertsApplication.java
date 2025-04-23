3c975a2171c185fba496a73fb295407dd86e0c46
Xajah <filho.antoine@gmail.com>
2025-04-23 18:21:21 +0200

    Spring project Initialisation

---
+++ src/main/java/com/openclassrooms/P_5_SafetyNet_Alerts/P5SafetyNetAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P_5_SafetyNet_Alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5SafetyNetAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5SafetyNetAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/P_5_SafetyNet_Alerts/P5SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P_5_SafetyNet_Alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P5SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
